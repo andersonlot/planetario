c9d11912ac1c92e2206b1bda4cf5ab3280344ce6
andersonlot <andersonlot.ee@gmail.com>
2022-09-14 14:51:11 -0300

    changing forces parameters

--- assets/sketch.js
+++ assets/sketch.js
@@ -1,25 +1,40 @@
 const planets = [];
+const max=60;
 
 function setup() {
-  canva=createCanvas(600, 600);
+  canva=createCanvas(900, 900);
   canva.parent('canvas');
   planets.push(new Planet(width * 0.75, height * 0.5));
+  sun=new Planet(width/2,height/2);
+  sun.c="#FFEE00";
+  sun.size = 5000;
+  sun.deltaX=0;
+}
+
+
+
+function draw() {
+  clear();
+  //background(10);
+  //sun.draw();
+  fill(255,255,20);
+  circle(sun.x,sun.y,50);
+
+  for (const planet of planets) {
+    planet.draw();
+    planet.check(sun);
+    //sun.check(planet);
+    for(const planetB of planets){
+    planet.check(planetB);
+    
+    }
+  }
 }
 
 function mousePressed() {
   planets.push(new Planet(mouseX, mouseY));
-}
-
-function draw() {
-  background(32);
-  fill(255, 255, 0);
-  circle(width / 2, height / 2, 50);
-
-  for (const planet of planets) {
-    planet.draw();
-    for(const planetB of planets){
-    planet.check(planetB);
-    }
+  if(planets.length>max){
+    planets.shift();
   }
 }
 
@@ -27,32 +42,25 @@ class Planet {
   constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.size = random(1, 20);
-    this.deltaX =0
-    this.deltaY = 0
+    this.size = random(1, 50);
+    this.deltaX = 0.1;
+    this.deltaY = 0;
     this.c = color(random(100, 255), random(100, 255), random(100, 255));
   }
 
   draw() {
-    const sunX = width / 2;
-    const sunY = height / 2;
-    const distanceSun = dist(this.x, this.y, sunX, sunY);
-    if(distanceSun>10){
-    this.deltaX += 50*(sunX - this.x) / (distanceSun*distanceSun*distanceSun);
-    this.deltaY += 50*(sunY - this.y) / (distanceSun*distanceSun*distanceSun);
-    }
 
-    this.x += 0.5*this.deltaX;
-    this.y += 0.5*this.deltaY;
+    this.x += 100*0.2*this.deltaX;
+    this.y += 100*0.2*this.deltaY;
 
     fill(this.c);
     ellipse(this.x, this.y, this.size);
   }
   check(planetB){
-    if(dist(this.x,this.y,planetB.x,planetB.y)<5){
+    if(dist(this.x,this.y,planetB.x,planetB.y)<10){
       return;
     }
-   this.deltaX += 1*planetB.size* (planetB.x - this.x) / (dist(this.x,this.y,planetB.x,planetB.y)*dist(this.x,this.y,planetB.x,planetB.y)*dist(this.x,this.y,planetB.x,planetB.y));
-   this.deltaY += 1*planetB.size*(planetB.y - this.y) / (dist(this.x,this.y,planetB.x,planetB.y)*dist(this.x,this.y,planetB.x,planetB.y)*dist(this.x,this.y,planetB.x,planetB.y));
+   this.deltaX += 1.5*0.01*planetB.size* (planetB.x - this.x) / (dist(this.x,this.y,planetB.x,planetB.y)*dist(this.x,this.y,planetB.x,planetB.y)*dist(this.x,this.y,planetB.x,planetB.y));
+   this.deltaY += 1.5*0.01*planetB.size*(planetB.y - this.y) / (dist(this.x,this.y,planetB.x,planetB.y)*dist(this.x,this.y,planetB.x,planetB.y)*dist(this.x,this.y,planetB.x,planetB.y));
   }
 }
