b6c9a540a27e6f029b1161d77478d7244b91621d
andersonlot <andersonlot.ee@gmail.com>
2022-09-14 14:58:05 -0300

    uploading picture

--- assets/sketch.js
+++ assets/sketch.js
@@ -1,6 +1,9 @@
 const planets = [];
 const max=60;
-
+var sunImg;
+function preload(){
+  sunImg= loadImage("https://andersonlot.github.io/planetario/assets/sun_image.png");
+}
 function setup() {
   canva=createCanvas(900, 900);
   canva.parent('canvas');
@@ -18,6 +21,7 @@ function draw() {
   //background(10);
   //sun.draw();
   fill(255,255,20);
+  image()
   circle(sun.x,sun.y,50);
 
   for (const planet of planets) {
@@ -43,15 +47,15 @@ class Planet {
     this.x = x;
     this.y = y;
     this.size = random(1, 50);
-    this.deltaX = 0.1;
+    this.deltaX = 5;
     this.deltaY = 0;
     this.c = color(random(100, 255), random(100, 255), random(100, 255));
   }
 
   draw() {
 
-    this.x += 100*0.2*this.deltaX;
-    this.y += 100*0.2*this.deltaY;
+    this.x += 1*0.2*this.deltaX;
+    this.y += 1*0.2*this.deltaY;
 
     fill(this.c);
     ellipse(this.x, this.y, this.size);
@@ -60,7 +64,7 @@ class Planet {
     if(dist(this.x,this.y,planetB.x,planetB.y)<10){
       return;
     }
-   this.deltaX += 1.5*0.01*planetB.size* (planetB.x - this.x) / (dist(this.x,this.y,planetB.x,planetB.y)*dist(this.x,this.y,planetB.x,planetB.y)*dist(this.x,this.y,planetB.x,planetB.y));
-   this.deltaY += 1.5*0.01*planetB.size*(planetB.y - this.y) / (dist(this.x,this.y,planetB.x,planetB.y)*dist(this.x,this.y,planetB.x,planetB.y)*dist(this.x,this.y,planetB.x,planetB.y));
+   this.deltaX += 50*0.01*planetB.size* (planetB.x - this.x) / (dist(this.x,this.y,planetB.x,planetB.y)*dist(this.x,this.y,planetB.x,planetB.y)*dist(this.x,this.y,planetB.x,planetB.y));
+   this.deltaY += 50*0.01*planetB.size*(planetB.y - this.y) / (dist(this.x,this.y,planetB.x,planetB.y)*dist(this.x,this.y,planetB.x,planetB.y)*dist(this.x,this.y,planetB.x,planetB.y));
   }
 }
