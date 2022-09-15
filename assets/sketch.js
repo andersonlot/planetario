e5cd66ab0d35a46132f0de034271779803868e85
andersonlot <andersonlot.ee@gmail.com>
2022-09-15 09:26:49 -0300

    mouse Well defines the size of new planets

--- assets/sketch.js
+++ assets/sketch.js
@@ -1,16 +1,19 @@
 const planets = [];
 const max=60;
 var sunImg;
+
+var newPlanetSize = 20;
+
 function preload(){
   sunImg= loadImage("https://andersonlot.github.io/planetario/assets/sun_image.png");
 }
 function setup() {
   canva=createCanvas(600, 600);
   canva.parent('canvas');
-  planets.push(new Planet(width * 0.75, height * 0.5));
-  sun=new Planet(width/2,height/2);
+  planets.push(new Planet(width * 0.75, height * 0.5,20));
+  sun=new Planet(width/2,height/2,1);
   sun.c="#FFEE00";
-  sun.size = 5000;
+  sun.mass = 5000;
   sun.deltaX=0;
 
 }
@@ -42,22 +45,31 @@ function draw() {
   noFill();
   rect(0,0,width,height);
   pop();
+
+  push();
+  noFill();
+  stroke(255,50);
+  drawingContext.setLineDash([newPlanetSize*0.05])
+  strokeWeight(3);
+  circle(mouseX, mouseY,newPlanetSize/2);
+  pop();
 }
 
 function mousePressed() {
-  planets.push(new Planet(mouseX, mouseY));
+  planets.push(new Planet(mouseX, mouseY,newPlanetSize));
   if(planets.length>max){
     planets.shift();
   }
 }
 
 class Planet {
-  constructor(x, y) {
+  constructor(x, y,size) {
     this.x = x;
     this.y = y;
-    this.size = random(1, 50);
-    this.deltaX = 5;
-    this.deltaY = 0;
+    this.size = size;
+    this.mass= this.size*this.size*this.size*0.008;
+    this.deltaX = 0; 
+    this.deltaY = 7;
     this.c = color(random(100, 255), random(100, 255), random(100, 255));
   }
 
@@ -67,13 +79,22 @@ class Planet {
     this.y += 1*0.2*this.deltaY;
 
     fill(this.c);
-    ellipse(this.x, this.y, this.size);
+    ellipse(this.x, this.y, this.size/2);
   }
   check(planetB){
-    if(dist(this.x,this.y,planetB.x,planetB.y)<10){
+    if(dist(this.x,this.y,planetB.x,planetB.y)<20){
       return;
     }
-   this.deltaX += 50*0.01*planetB.size* (planetB.x - this.x) / (dist(this.x,this.y,planetB.x,planetB.y)*dist(this.x,this.y,planetB.x,planetB.y)*dist(this.x,this.y,planetB.x,planetB.y));
-   this.deltaY += 50*0.01*planetB.size*(planetB.y - this.y) / (dist(this.x,this.y,planetB.x,planetB.y)*dist(this.x,this.y,planetB.x,planetB.y)*dist(this.x,this.y,planetB.x,planetB.y));
+   this.deltaX += 50 * 0.01 * planetB.mass * (planetB.x - this.x) / (dist(this.x,this.y,planetB.x,planetB.y)*dist(this.x,this.y,planetB.x,planetB.y)*dist(this.x,this.y,planetB.x,planetB.y));
+   this.deltaY += 50 * 0.01 * planetB.mass * (planetB.y - this.y) / (dist(this.x,this.y,planetB.x,planetB.y)*dist(this.x,this.y,planetB.x,planetB.y)*dist(this.x,this.y,planetB.x,planetB.y));
   }
+}
+
+function mouseWheel(event) {
+  if ( mouseX>0 && mouseY>0 && mouseX<width && mouseY<height){
+  print(event.delta);
+  newPlanetSize += 0.1*event.delta;
+  return false;
+  }
+  return true;
 }
